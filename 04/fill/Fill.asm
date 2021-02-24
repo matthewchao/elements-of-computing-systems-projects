b20d8240a45b75f912066a6ee57e536cb243f7d5
Matthew Chao <matthew.chao1@yahoo.com>
2021-02-23 22:41:37 -0500

    Plan basic structure for Fill program

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,27 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+
+// Scans the keyboard address for anything nonzero
+
+// Ram[0] = 0 if on the last scan, there were no keys pressed
+// Ram[0] = 1 if on the last scan, there WAS a key pressed
+// Ram[1] equals the status that we are changing to
+
+
+
+(SCAN)
+// Checks if the status R0 should change
+// Jump to SCAN again if the status hasn't changed
+// Otherwise set the status (either Ram[0] = 0 or Ram[0]=1)
+// If Ram[0]=0, then Jump to the Unfill routine
+// Otherwise proceed to the Fill routine
+
+(FILL)
+// Fill the screen
+// Jump to SCAN
+
+(UNFILL)
+// Clear the screen
+// Jump to SCAN
